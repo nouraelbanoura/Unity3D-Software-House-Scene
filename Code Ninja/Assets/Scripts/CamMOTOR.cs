f51a620cdc2e2567b83f9f3db96443d8e252619f
nouraelbanoura <noura.elsaadany@student.guc.edu.eg>
2018-11-20 23:52:21 +0200

    Unity files

---
+++ Code Ninja/Assets/Scripts/CamMOTOR.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+
+public class CamMOTOR : MonoBehaviour,IDragHandler,IPointerUpHandler,IPointerDownHandler {
+    private Image backGround;
+    private Image joyStickImage;
+    private Vector3 inputVector;
+
+    private void Start()
+    {
+        backGround = GetComponent<Image>();
+        joyStickImage = transform.GetChild(0).GetComponent<Image>();
+    }
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(backGround.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        
+            Debug.Log("heey");
+        
+
+
+    }
+
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+
+    }
+    
+    }
+
---
+++ Code Ninja/Assets/Scripts/fan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fan : MonoBehaviour {
+    float spingSpeed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0,0 , spingSpeed * Time.deltaTime);	
+	}
+}
